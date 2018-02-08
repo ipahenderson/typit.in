0019cdbaa8c6155f27dc412e5dc1d2782c283eff
ipahenderson <ipahenderson@gmail.com>
2018-02-08 15:00:22 +0000

    back end file structure created

---
+++ back_end/server.js
@@ -0,0 +1,31 @@
+
+var express = require("express");
+var server = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const objectID = require('mongodb').ObjectID;
+server.use(cors());
+server.use(express.static('front_end/build'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended:true}));
+
+
+MongoClent.connect("mongoldb://localhost:27017", function(){
+	if(err){
+		console.log(err)
+	return;
+})
+
+server.get("api/words", function(req, res){
+d.collection("typit_words").find().toArray(function(err, result){
+res.status(200);
+res.json(result);
+});
+})
+
+server.use(express.static("build"));
+
+server.listen(5000, function(){
+ console.log(‘Typit Backend running on port: ’ + this.address().port);
+});
