435c2fb8486f24a7ddf458c620cd963343044e45
AlistairAlistair <email@alistairguthrie.co>
2018-02-09 16:25:23 +0000

    seeds created

---
+++ back_end/seeds/words_seeds.js
@@ -0,0 +1,228 @@
+db.words.insert(
+//foods
+{
+  word: "apple",
+  category: "food",
+  image: "src/images/food/apple.svg"
+},
+{
+  word: "banana",
+  category: "food",
+  image: "src/images/food/banana.svg"
+},
+{
+  word: "bread",
+  category: "food",
+  image: "src/images/food/bread.svg"
+},
+{
+  word: "burger",
+  category: "food",
+  image: "src/images/food/burger.svg"
+},
+{
+  word: "carrot",
+  category: "food",
+  image: "src/images/food/carrot.svg"
+},
+{
+  word: "cheese",
+  category: "food",
+  image: "src/images/food/cheese.svg"
+},
+{
+  word: "egg",
+  category: "food",
+  image: "src/images/food/egg.svg"
+},
+{
+  word: "lemon",
+  category: "food",
+  image: "src/images/food/lemon.svg"
+},
+{
+  word: "orange",
+  category: "food",
+  image: "src/images/food/orange.svg"
+},
+{
+  word: "pizza",
+  category: "food",
+  image: "src/images/food/pizza.svg"
+},
+{
+  word: "strawberry",
+  category: "food",
+  image: "src/images/food/strawberry.svg"
+},
+{
+  word: "tomato",
+  category: "food",
+  image: "src/images/food/tomato.svg"
+},
+//colours
+{
+  word: "yellow",
+  category: "colour",
+  image: "src/images/colours/yellow.svg"
+},
+{
+  word: "red",
+  category: "colour",
+  image: "src/images/colours/red.svg"
+},
+{
+  word: "green",
+  category: "colour",
+  image: "src/images/colours/green.svg"
+},
+{
+  word: "blue",
+  category: "colour",
+  image: "src/images/colours/blue.svg"
+},
+{
+  word: "pink",
+  category: "colour",
+  image: "src/images/colours/pink.svg"
+},
+{
+  word: "orange",
+  category: "colour",
+  image: "src/images/colours/orange.svg"
+},
+{
+  word: "white",
+  category: "colour",
+  image: "src/images/colours/white.svg"
+},
+{
+  word: "purple",
+  category: "colour",
+  image: "src/images/colours/purple.svg"
+},
+{
+  word: "black",
+  category: "colour",
+  image: "src/images/colours/black.svg"
+},
+{
+  word: "brown",
+  category: "colour",
+  image: "src/images/colours/brown.svg"
+},
+
+//animals
+{
+  word: "cat",
+  category: "animal",
+  image: "src/images/animals/cat.svg"
+},
+{
+  word: "dog",
+  category: "animal",
+  image: "src/images/animals/dog.svg"
+},
+
+{
+  word: "elephant",
+  category: "animal",
+  image: "src/images/animals/elephant.svg"
+},
+
+{
+  word: "fish",
+  category: "animal",
+  image: "src/images/animals/fish.svg"
+},
+
+{
+  word: "hedgehog",
+  category: "animal",
+  image: "src/images/animals/hedghog.svg"
+},
+
+{
+  word: "horse",
+  category: "animal",
+  image: "src/images/animals/horse.svg"
+},
+
+{
+  word: "parrot",
+  category: "animal",
+  image: "src/images/animals/parrot.svg"
+},
+
+{
+  word: "pig",
+  category: "animal",
+  image: "src/images/animals/pig.svg"
+},
+
+{
+  word: "rabbit",
+  category: "animal",
+  image: "src/images/animals/rabbit.svg"
+},
+
+{
+  word: "sheep",
+  category: "animal",
+  image: "src/images/animals/sheep.svg"
+},
+
+//clothing
+
+{
+  word: "coat",
+  category: "clothing",
+  image: "src/images/clothing/coat.svg"
+},
+{
+  word: "gloves",
+  category: "clothing",
+  image: "src/images/clothing/gloves.svg"
+},
+{
+  word: "hat",
+  category: "clothing",
+  image: "src/images/clothing/hat.svg"
+},
+{
+  word: "scarf",
+  category: "clothing",
+  image: "src/images/clothing/scarf.svg"
+},
+{
+  word: "shirt",
+  category: "clothing",
+  image: "src/images/clothing/shirt.svg"
+},
+{
+  word: "shoe",
+  category: "clothing",
+  image: "src/images/clothing/shoe.svg"
+},
+{
+  word: "skirt",
+  category: "clothing",
+  image: "src/images/clothing/skirt.svg"
+},
+{
+  word: "socks",
+  category: "clothing",
+  image: "src/images/clothing/socks.svg"
+},
+{
+  word: "tie",
+  category: "clothing",
+  image: "src/images/clothing/tie.svg"
+},
+{
+  word: "trousers",
+  category: "clothing",
+  image: "src/images/clothing/trousers.svg"
+}
+
+);
