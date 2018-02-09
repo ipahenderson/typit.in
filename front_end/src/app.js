ba5c9e761ccd1d085e9347ced5466c05e0ee6a40
fraser1234 <fraseralexanderblack@gmail.com>
2018-02-09 15:47:19 +0000

    running on app.js

---
+++ front_end/src/app.js
@@ -0,0 +1,7 @@
+const keyPress = require ('./models/keyPress');
+// const KeyBoard = require ('./models/keyboard');
+
+window.addEventListener('load', function(){
+  keyPress();
+
+})
---
+++ front_end/src/models/keyPress.js
@@ -0,0 +1,421 @@
+const KeyBoard = require('./keyboard.js');
+
+
+const startKeyListener = function(){
+
+const keyBoard = new KeyBoard();
+
+document.addEventListener('keydown', function(){
+
+  // var printKeyZero = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "0"; // could just pass in the key here to it can be the same fucntion for any key press
+  //   clicked.appendChild(text);
+  // };
+  //
+  // var printKeyOne = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "1";
+  //   clicked.appendChild(text);
+  // };
+  //
+  // var printKeyTwo = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "2";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyThree = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "3";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyFour = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "4";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyFive = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "5";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeySix = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "6";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeySeven = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "7";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyEight = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "8";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyNine = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "9";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyA = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "A";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyB = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "B";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyC = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "C";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyD = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "D";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyE = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "E";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyF = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "F";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyG = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "G";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyH = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "H";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyI = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "I";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyJ = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "J";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyK = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "K";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyL = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "L";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyM = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "M";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyN = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "N";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyO = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "O";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyP = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "P";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyQ = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "Q";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyR = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "R";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyS = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "S";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyT = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "T";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyU = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "U";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyV = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "V";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyW = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "W";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyX = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "X";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyY = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "Y";
+  //   clicked.appendChild(text);
+  // };
+  // var printKeyZ = function(){
+  //   // if key is pressed, got to get the id of the p tag where it is destined to go
+  //   // append it to the p tag
+  //   const clicked = document.querySelector('#answer-display');
+  //   const text = document.createElement('p');
+  //   text.innerText = "Z";
+  //   clicked.appendChild(text);
+  // };
+
+
+  function keyPress(e){
+    // keyBoard.addKey(e.key);
+    if(e.keyCode == 48){
+
+      keyBoard.addKey(e.key);
+      console.log(keyBoard.pressedKeys);
+      // printKeyZero();
+      //('#id of key').css("background", "red")
+    }
+    if(e.keyCode == 49){ // 1
+      printKeyOne();
+    }
+    if(e.keyCode == 50){ // 2
+      printKeyTwo();
+    }
+    if(e.keyCode == 51){ // 3
+      printKeyThree();
+    }
+    if(e.keyCode == 52){ // 4
+      printKeyFour();
+    }
+    if(e.keyCode == 53){ // 5
+      printKeyFive();
+    }
+    if(e.keyCode == 54){ // 6
+      printKeySix();
+    }
+    if(e.keyCode == 55){ // 7
+      printKeySeven();
+    }
+    if(e.keyCode == 56){ // 8
+      printKeyEight();
+    }
+    if(e.keyCode == 57){ // 9
+      printKeyNine();
+    }
+    if(e.keyCode == 65){ // a
+      printKeyA();
+    }
+    if(e.keyCode == 66){ // b
+      printKeyB();
+    }
+    if(e.keyCode == 67){ // c
+      printKeyC();
+    }
+    if(e.keyCode == 68){ // d
+      printKeyD();
+    }
+    if(e.keyCode == 69){ // e
+      printKeyE();
+    }
+    if(e.keyCode == 70){ // f
+      printKeyF();
+    }
+    if(e.keyCode == 71){ // g
+      printKeyG();
+    }
+    if(e.keyCode == 72){ // h
+      printKeyH();
+    }
+    if(e.keyCode == 73){ // i
+      printKeyI();
+    }
+    if(e.keyCode == 74){ // j
+      printKeyJ();
+    }
+    if(e.keyCode == 75){ // k
+      printKeyK();
+    }
+    if(e.keyCode == 76){ // l
+      printKeyL();
+    }
+    if(e.keyCode == 77){ // m
+      printKeyM();
+    }
+    if(e.keyCode == 78){ // n
+      printKeyN();
+    }
+    if(e.keyCode == 79){ // o
+      printKeyO();
+    }
+    if(e.keyCode == 80){ // p
+      printKeyP();
+    }
+    if(e.keyCode == 81){ // q
+      printKeyQ();
+    }
+    if(e.keyCode == 82){ // r
+      printKeyR();
+    }
+    if(e.keyCode == 83){ // s
+      printKeyS();
+    }
+    if(e.keyCode == 84){ // t
+      printKeyT();
+    }
+    if(e.keyCode == 85){ // u
+      printKeyU();
+    }
+    if(e.keyCode == 86){ // v
+      printKeyV();
+    }
+    if(e.keyCode == 87){ // w
+      printKeyW();
+    }
+    if(e.keyCode == 88){ // x
+      printKeyX();
+    }
+    if(e.keyCode == 89){ // y
+      printKeyY();
+    }
+    if(e.keyCode == 90){ // z
+      printKeyZ();
+    }
+
+  };
+  document.onkeydown = keyPress;
+})
+};
+module.exports = startKeyListener;
