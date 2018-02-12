e7dca8fa12e5104d5c99170375705592f8f0b24c
ipahenderson <ipahenderson@gmail.com>
2018-02-12 16:30:05 +0000

    button setup

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -17,7 +17,6 @@ const app = function(){
   const foodButton = document.querySelector('#food-game-button');
   const testButton = document.querySelector('#test-game-button');
 
-
   wordsData.getData();
 
   var deleteButtons = function(){
@@ -26,9 +25,7 @@ const app = function(){
     foodButton.parentNode.removeChild(foodButton);
     colourButton.parentNode.removeChild(colourButton);
     testButton.parentNode.removeChild(testButton);
-
   }
-
 
   animalButton.addEventListener('click', function(){
     deleteButtons();
@@ -122,6 +119,11 @@ const app = function(){
     speakButton.addEventListener('click', function(){
       responsiveVoice.speak(words.word)
     });
+
+
+    
+
+
 
   });
 
