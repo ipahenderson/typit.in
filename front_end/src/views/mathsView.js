0499d7cf333fb5e8171086a08cb676ba4f96a3e2
ipahenderson <ipahenderson@gmail.com>
2018-02-13 15:44:43 +0000

    css for numbers game sorted

--- front_end/src/views/mathsView.js
+++ front_end/src/views/mathsView.js
@@ -35,31 +35,39 @@ MathsView.prototype.render = function(data, roundCount, totalRounds){
   var image1 = document.createElement('img');
   image1.id = "game-image1";
   image1.src = data.image1;
-  image1.style.cssText = "width: 75px;height: 75px;";
+  image1.style.cssText = "width: 110px;height: 110px; margin: 110px 0px 0px 0px";
 
   var image2 = document.createElement('img');
   image2.id = "game-image2";
   image2.src = data.image2;
-  image2.style.cssText = "width: 75px;height: 75px;";
+  image2.style.cssText = "width: 70px;height: 70px; margin: 110px 30px 20px 30px;";
 
   var image3 = document.createElement('img');
   image3.id = "game-image3";
   image3.src = data.image3;
-  image3.style.cssText = "width: 75px;height: 75px;";
+  image3.style.cssText = "width: 110px;height: 110px; margin: 110px 0px 0px 0px";
+
+  var equals = document.createElement('img');
+  equals.id = "equals";
+  equals.src = "/images/maths/equal.svg";
+  equals.style.cssText = "width: 70px;height: 70px; margin:110px 30px 20px 30px;";
 
   var image4 = document.createElement('img');
   image4.id = "game-image4";
   image4.src = data.image4;
-  image4.style.cssText = "width: 75px;height: 75px;";
+  image4.style.cssText = "width: 110px;height: 110px;margin: 110px 0px 0px 0px;";
   image4.classList.add('hide');
 
   var counter = document.createElement('p');
   counter.id = "counter-display";
   counter.innerText = `${(roundCount + 1)} of  ${totalRounds}`;
+  counter.style.cssText = "margin-top: 95px;";
+
 
   this.container.appendChild(image1);
   this.container.appendChild(image2);
   this.container.appendChild(image3);
+  this.container.appendChild(equals);
   this.container.appendChild(image4);
   this.container.appendChild(counter);
 }
